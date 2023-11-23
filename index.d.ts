82bff30dac065732d19fae2b56ad9c790a315ad7
sullayang(杨金伟) <sullayang@futunn.com>
2023-11-23 14:38:12 +0800

    feat: 删除打包产物

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Plugin } from 'vite';
-type EnvList = string[];
-type EnvMap = Record<string, any>;
-export declare function VitePluginEnv(envs: EnvList | EnvMap | undefined): Plugin;
-export {};
